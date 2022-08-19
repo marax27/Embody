4f9f4a37501a87dfa72ea0ea499ce20e9827c745
Kacper Tonia <kacpert314@gmail.com>
2022-08-20 01:58:21 +0200

    Implement length and "+-" operators

--- src/Embody/LinearAlgebra.fs
+++ src/Embody/LinearAlgebra.fs
@@ -10,10 +10,28 @@ module LinearAlgebra =
             z: float<'u>
         }
 
+        static member (+) (v, u) = {
+            x = v.x + u.x
+            y= v.y + u.y
+            z = v.z + u.z
+        }
+
+        static member (-) (v, u) = {
+            x = v.x - u.x
+            y = v.y - u.y
+            z = v.z - u.z
+        }
+
     /// Construct an actual 3D vector.
     let inline vector3 x y z = { x = x; y = y; z = z }
 
     /// A module which implements vector operations.
     module Vector3 =
-        let foo () =
-            123
+
+        /// Compute squared length (squared magnitude) of a vector.
+        let inline squaredLength vector =
+            vector.x * vector.x + vector.y * vector.y + vector.z * vector.z
+
+        let inline length (vector: NumericalVector3<'u>): float<'u> =
+            let result = (squaredLength vector |> float) ** 0.5
+            result |> LanguagePrimitives.FloatWithMeasure
--- tests/Embody.Tests/LinearAlgebraTests.fs
+++ tests/Embody.Tests/LinearAlgebraTests.fs
@@ -33,3 +33,59 @@ module ``When creating a vector3`` =
         Assert.Equal(5.0, float sut.x)
         Assert.Equal(10.0, float sut.y)
         Assert.Equal(2.0, float sut.z)
+
+
+module ``When calculating vector3's properties`` =
+    
+    [<Measure>] type U
+
+    [<Fact>]
+    let ``Given sample vector, Then it has expected squared length`` () =
+        
+        let expected = 121.0
+        let sut = vector3 2.0 6.0 9.0
+
+        let actual = sut |> Vector3.squaredLength 
+
+        Assert.Equal(actual, expected)
+
+    [<Fact>]
+    let ``Given sample vector, Then it has expected length`` () =
+        
+        let expected = 11.0<U>
+        let sut = vector3 2.0<U> 6.0<U> 9.0<U>
+
+        let actual = sut |> Vector3.length
+
+        Assert.Equal(actual, expected)
+
+
+module ``When performing binary vector3 operations`` =
+
+    let SampleVector = vector3 5.0 -1.0 2.0
+    let OtherVector = vector3 -15.0 -1.0 1.0
+
+    [<Fact>]
+    let ``Given 2 vectors (Sample, Other), Then `Sample + Other` returns expected vector`` () =
+        let expected = vector3 -10.0 -2.0 3.0
+        let actual = SampleVector + OtherVector
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ``Given 2 vectors (Sample, Other), Then `Other + Sample` returns expected vector`` () =
+        let expected = vector3 -10.0 -2.0 3.0
+        let actual = OtherVector + SampleVector
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ``Given 2 vectors (Sample, Other), Then `Sample - Other` returns expected vector`` () =
+        let expected = vector3 20.0 0.0 1.0
+        let actual = SampleVector - OtherVector
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ``Given 2 vectors (Sample, Other), Then `Other - Sample` returns expected vector`` () =
+        let expected = vector3 -20.0 0.0 -1.0
+        let actual = OtherVector - SampleVector
+        Assert.Equal(expected, actual)
+
