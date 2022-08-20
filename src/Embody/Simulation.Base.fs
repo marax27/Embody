9a4458bee7e9b7d264ea987cfdc42eb67e3b5324
Kacper Tonia <kacpert314@gmail.com>
2022-08-20 21:28:49 +0200

    Add Simulation and Metrics

---
+++ src/Embody/Simulation.Base.fs
@@ -0,0 +1,24 @@
+﻿namespace Embody.Simulation
+
+module Base =
+
+    open Embody.Domain
+    open Embody.Integration
+
+    /// Contains data necessary to run a simulation, such as initial conditions and integration settings.
+    type SimulationPreset<[<Measure>] 'l, [<Measure>] 't, [<Measure>] 'm> = {
+        Settings: IntegratorSettings<'l, 't, 'm>
+        System: CelestialSystem<'l, 't, 'm>
+        Integrator: Integrator<'l, 't, 'm>
+    }
+
+    /// Contains results of a simulation (integration steps). Also stores the simulation's preset.
+    type SimulationResult<[<Measure>] 'l, [<Measure>] 't, [<Measure>] 'm> = {
+        Preset: SimulationPreset<'l, 't, 'm>
+        Steps: IntegratorStep<'l, 't> array
+    }
+
+    /// Simulation metric is a function that assigns a value to each simulation step.
+    type NumericalMetric<[<Measure>] 'l, [<Measure>] 't, [<Measure>] 'm, [<Measure>] 'metric> =
+        SimulationPreset<'l, 't, 'm> -> IntegratorStep<'l, 't>
+            -> float<'metric>
