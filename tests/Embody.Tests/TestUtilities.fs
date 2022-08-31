b1a976ea9b34a38138f520918a73b8117704a3e9
Kacper Tonia <kacpert314@gmail.com>
2022-09-01 00:44:46 +0200

    Create TransformsTests

---
+++ tests/Embody.Tests/TestUtilities.fs
@@ -0,0 +1,14 @@
+﻿namespace Tests
+
+
+module Utilities =
+
+    open Xunit
+    open FsUnit.Xunit
+
+    /// Check if 2 arrays are equal. It is a composition of available FsUnit/Xunit assertions.
+    let shouldBeEqualTo (expected: 'a array) (actual: 'a array) =
+
+        actual |> should haveLength (expected |> Array.length)
+
+        (Array.forall2 (=) expected actual) |> should be True
